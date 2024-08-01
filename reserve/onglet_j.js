f050951a25ffd526e10c28a8c6a8d8fa4ad9afc7
TOGO-Ar <togoarouna31103@gmail.com>
2024-08-01 05:34:09 +0200

    Add files via upload

---
+++ reserve/onglet_j.js
@@ -0,0 +1,7 @@
+function ligne_menu() {
+  var menu = document.getElementById('menu');
+  menu.classList.toggle('active');
+
+  var menuToggle = document.querySelector('.menu_ligne');
+  menuToggle.classList.toggle('active');
+}
